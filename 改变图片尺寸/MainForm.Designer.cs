b70d22797da917086c5972a6372119978c49fb53
雨天 <276830175@qq.com>
2024-08-16 13:53:22 +0800

    添加项目文件。

---
+++ 改变图片尺寸/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace 改变图片尺寸
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textLength = new System.Windows.Forms.TextBox();
+            this.textWidth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textHeight = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.butSubmit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labWorkPath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // butOpen
+            // 
+            this.butOpen.Location = new System.Drawing.Point(83, 36);
+            this.butOpen.Name = "butOpen";
+            this.butOpen.Size = new System.Drawing.Size(75, 23);
+            this.butOpen.TabIndex = 0;
+            this.butOpen.Text = "打开文件夹";
+            this.butOpen.UseVisualStyleBackColor = true;
+            this.butOpen.Click += new System.EventHandler(this.butOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "长：";
+            // 
+            // textLength
+            // 
+            this.textLength.Location = new System.Drawing.Point(83, 106);
+            this.textLength.Name = "textLength";
+            this.textLength.Size = new System.Drawing.Size(100, 21);
+            this.textLength.TabIndex = 2;
+            // 
+            // textWidth
+            // 
+            this.textWidth.Location = new System.Drawing.Point(83, 133);
+            this.textWidth.Name = "textWidth";
+            this.textWidth.Size = new System.Drawing.Size(100, 21);
+            this.textWidth.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "宽：";
+            // 
+            // textHeight
+            // 
+            this.textHeight.Location = new System.Drawing.Point(83, 160);
+            this.textHeight.Name = "textHeight";
+            this.textHeight.Size = new System.Drawing.Size(100, 21);
+            this.textHeight.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "高：";
+            // 
+            // butSubmit
+            // 
+            this.butSubmit.Location = new System.Drawing.Point(83, 200);
+            this.butSubmit.Name = "butSubmit";
+            this.butSubmit.Size = new System.Drawing.Size(75, 23);
+            this.butSubmit.TabIndex = 7;
+            this.butSubmit.Text = "制作";
+            this.butSubmit.UseVisualStyleBackColor = true;
+            this.butSubmit.Click += new System.EventHandler(this.butSubmit_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "cm";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "cm";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(189, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "cm";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 77);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 12);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "工作路径：";
+            // 
+            // labWorkPath
+            // 
+            this.labWorkPath.AutoSize = true;
+            this.labWorkPath.Location = new System.Drawing.Point(81, 77);
+            this.labWorkPath.Name = "labWorkPath";
+            this.labWorkPath.Size = new System.Drawing.Size(113, 12);
+            this.labWorkPath.TabIndex = 12;
+            this.labWorkPath.Text = "请选择或拖入文件夹";
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 285);
+            this.Controls.Add(this.labWorkPath);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.butSubmit);
+            this.Controls.Add(this.textHeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textLength);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.butOpen);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "图片修改";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textLength;
+        private System.Windows.Forms.TextBox textWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button butSubmit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labWorkPath;
+    }
+}
+
---
+++ 改变图片尺寸/MainForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 改变图片尺寸
+{
+    public partial class MainForm : Form
+    {
+        string _workPath;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void butOpen_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.SelectedPath = System.Environment.CurrentDirectory;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    _workPath = dialog.SelectedPath;
+                    labWorkPath.Text =
+                        _workPath.Length < 30 ? _workPath : _workPath.Substring(0, 10) + "……" + _workPath.Substring(_workPath.Length - 10);
+                }
+            }
+        }
+
+        private void butSubmit_Click(object sender, EventArgs e)
+        {
+            if (_workPath == null)
+            {
+                MessageBox.Show("先选择文件夹");
+                return;
+            }
+
+            string outPath = Path.Combine(_workPath, "新文件");
+            if (!Directory.Exists(outPath))
+            {
+                Directory.CreateDirectory(outPath);
+            }
+
+            try
+            {
+                double length = Convert.ToDouble(textLength.Text);
+                double width = Convert.ToDouble(textWidth.Text);
+                double height = Convert.ToDouble(textHeight.Text);
+
+                int lengthPix = (int)(length * 118.11);
+                int widthPix = (int)(width * 118.11);
+                int heightPix = (int)(height * 118.11);
+                ChangeSize("1主视图.jpg", lengthPix, heightPix, outPath);
+                ChangeSize("2后视图.jpg", lengthPix, heightPix, outPath);
+                ChangeSize("3左视图.jpg", widthPix, heightPix, outPath);
+                ChangeSize("4右视图.jpg", widthPix, heightPix, outPath);
+                ChangeSize("5俯视图.jpg", lengthPix, widthPix, outPath);
+                ChangeSize("6仰视图.jpg", lengthPix, widthPix, outPath);
+
+                MessageBox.Show("完成");
+            }
+            catch
+            {
+                MessageBox.Show("操作有误");
+            }
+        }
+
+        private void ChangeSize(string fileName, int newWidth, int newHeight, string outPath)
+        {
+            string srcFile = Path.Combine(_workPath, fileName);
+            using (Bitmap src = new Bitmap(srcFile), dest = new Bitmap(newWidth, newHeight))
+            {
+                using (Graphics g = Graphics.FromImage(dest))
+                {
+                    g.DrawImage(src, new Rectangle(0, 0, newWidth, newHeight));
+                }
+
+                string outFile = Path.Combine(outPath, fileName);
+                dest.SetResolution(300, 300);
+                dest.Save(outFile, ImageFormat.Jpeg);
+            }
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            // 如果有文件被拖入，改变拖放效果
+            e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop)
+                ? DragDropEffects.Copy
+                : DragDropEffects.None;
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files.Length != 1)
+            {
+                labWorkPath.Text = "拖入不是文件夹";
+                return;
+            }
+
+            string file = files[0];
+            if (!Directory.Exists(file))
+            {
+                labWorkPath.Text = "拖入不是文件夹";
+                return;
+            }
+
+            _workPath = file;
+            labWorkPath.Text =
+                _workPath.Length < 30 ? _workPath : _workPath.Substring(0, 10) + "……" + _workPath.Substring(_workPath.Length - 10);
+        }
+    }
+}
