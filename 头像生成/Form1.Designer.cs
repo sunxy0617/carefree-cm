eaa613d994e12f614109da30583ca30855bf9eec
雨天 <276830175@qq.com>
2024-11-29 17:33:00 +0800

    添加头像生成器

---
+++ 头像生成/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace 头像生成
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.butMake = new System.Windows.Forms.Button();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::头像生成.Properties.Resources.无忧背景;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(40, 562);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("华文隶书", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(123, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 97);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "忧";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("华文隶书", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(69, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 97);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "无";
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::头像生成.Properties.Resources.无忧背景;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(590, 679);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 400);
+            this.panel2.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("华文隶书", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(132, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 97);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "雨";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("华文隶书", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(72, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 97);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "风";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("华文隶书", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(72, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 97);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "听";
+            // 
+            // butMake
+            // 
+            this.butMake.Location = new System.Drawing.Point(240, 12);
+            this.butMake.Name = "butMake";
+            this.butMake.Size = new System.Drawing.Size(75, 23);
+            this.butMake.TabIndex = 2;
+            this.butMake.Text = "button1";
+            this.butMake.UseVisualStyleBackColor = true;
+            this.butMake.Click += new System.EventHandler(this.butMake_Click);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(110, 13);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(115, 21);
+            this.textName.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(158, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 474);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.butMake);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button butMake;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 头像生成/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using 头像生成.Properties;
+
+namespace 头像生成
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butMake_Click(object sender, EventArgs e)
+        {
+            string name = textName.Text;
+            pictureBox1.Image = MakeHead(name);
+        }
+
+        private Image MakeHead(string name)
+        {
+            using var backImage = Resources.无忧背景;
+            var image = new Bitmap(400, 400);
+            using var g = Graphics.FromImage(image);
+            g.DrawImage(backImage, 0, 0, 400, 400);
+            if (name.Length == 2)
+            {
+                string str1 = name.Substring(0, 1);
+                string str2 = name.Substring(1,1);
+                g.DrawString(str1, new Font("华文隶书", 72), Brushes.Black, 69, 84);
+                g.DrawString(str2, new Font("华文隶书", 72), Brushes.Black, 123, 181);
+            }
+            else if (name.Length == 3)
+            {
+                string str1 = name.Substring(0, 1);
+                string str2 = name.Substring(1,1);
+                string str3 = name.Substring(2,1);
+                g.DrawString(str1, new Font("华文隶书", 72), Brushes.Black,72, 69);
+                g.DrawString(str2, new Font("华文隶书", 72), Brushes.Black, 72, 152);
+                g.DrawString(str3, new Font("华文隶书", 72), Brushes.Black, 132, 225);
+            }
+
+            return image;
+        }
+    }
+}
